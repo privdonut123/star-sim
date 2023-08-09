452a9c78c8804d72a0db1897780f7713719bf73c
Barak Schmookler <bschmookler1@gmail.com>
2023-08-09 17:03:08 -0400

    Updated simulation

---
+++ simulation/starsim.pythia8_filter.C
@@ -0,0 +1,243 @@
+// macro to instantiate the Geant3 from within
+// STAR  C++  framework and get the starsim prompt
+// To use it do
+//  root4star starsim.C
+
+class St_geant_Maker;
+St_geant_Maker *geant_maker = 0;
+
+class StarGenEvent;
+StarGenEvent   *event       = 0;
+
+class StarPrimaryMaker;
+StarPrimaryMaker *_primary = 0;
+
+class StarFilterMaker;
+StarFilterMaker *filter = 0;
+
+// ----------------------------------------------------------------------------
+void geometry( TString tag, Bool_t agml=true )
+{
+  TString cmd = "DETP GEOM "; cmd += tag;
+  if ( !geant_maker ) geant_maker = (St_geant_Maker *)chain->GetMaker("geant");
+  geant_maker -> LoadGeometry(cmd);
+  //  if ( agml ) command("gexec $STAR_LIB/libxgeometry.so");
+}
+// ----------------------------------------------------------------------------
+void command( TString cmd )
+{
+  if ( !geant_maker ) geant_maker = (St_geant_Maker *)chain->GetMaker("geant");
+  geant_maker -> Do( cmd );
+}
+// ----------------------------------------------------------------------------
+// trig()  -- generates one event
+// trig(n) -- generates n+1 events.
+//
+// NOTE:  last event generated will be corrupt in the FZD file
+//
+void trig( Int_t n=1 )
+{
+  chain->EventLoop(n);
+  _primary->event()->Print();
+  
+}
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+void Pythia8( TString config="pp:W", Double_t ckin3=0.0, Double_t ckin4=-1.0 )
+{
+
+  //
+  // Create the pythia 8 event generator and add it to 
+  // the primary generator
+  //
+  StarPythia8 *pythia8 = new StarPythia8();    
+  if ( config=="pp:W" )
+    {
+      pythia8->SetFrame("CMS", 510.0);
+      pythia8->SetBlue("proton");
+      pythia8->SetYell("proton");
+      
+      pythia8->Set("WeakSingleBoson:all=off");
+      pythia8->Set("WeakSingleBoson:ffbar2W=on");
+      pythia8->Set("24:onMode=0");              // switch off all W+/- decaus
+      pythia8->Set("24:onIfAny 11 -11");        // switch on for decays to e+/-
+      
+    }
+  if ( config=="pp:minbias" )
+    {
+      pythia8->SetFrame("CMS", 500.0);
+      pythia8->SetBlue("proton");
+      pythia8->SetYell("proton");    
+
+      pythia8->Set("HardQCD:all = on");
+
+    }
+  if ( config=="pp:heavyflavor:D0jets" ) 
+    {
+      pythia8->Set("HardQCD:gg2ccbar = on");
+      pythia8->Set("HardQCD:qqbar2ccbar = on");
+      pythia8->Set("Charmonium:all = on");
+
+      pythia8->Set("421:mayDecay = 0");
+
+      pythia8->SetFrame("CMS", 200.0);
+      pythia8->SetBlue("proton");
+      pythia8->SetYell("proton");
+
+    }
+
+  // Setup phase space cuts
+  pythia8 -> Set(Form("PhaseSpace:ptHatMin=%f", ckin3 ));
+  pythia8 -> Set(Form("PhaseSpace:ptHatMax=%f", ckin4 ));
+
+  _primary -> AddGenerator( pythia8 );
+  
+}
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+void starsim( Int_t nevents=10, Int_t rngSeed=1234 )
+{ 
+
+  gROOT->ProcessLine(".L bfc.C");
+  {
+    //TString simple = "y2014x geant gstar usexgeom agml sdt20140530 DbV20150316 misalign ";
+    TString simple = "y2023 geant gstar usexgeom agml ";
+    bfc(0, simple );
+  }
+
+  gSystem->Load( "libVMC.so");
+
+  gSystem->Load( "StarGeneratorUtil.so");
+  gSystem->Load( "StarGeneratorEvent.so");
+  gSystem->Load( "StarGeneratorBase.so" );
+
+  gSystem->Load( "Pythia8_3_03.so"  );
+
+  gSystem->Load( "libMathMore.so"   );  
+
+  // Force loading of xgeometry
+  gSystem->Load( "xgeometry.so"     );
+
+  gSystem->Load("$OPTSTAR/lib/libfastjet.so");
+  gSystem->Load( "StarGeneratorFilt.so" );
+  gSystem->Load( "FastJetFilter.so" );
+  gSystem->Load( "FCSJetFilter.so" );
+
+//   // And unloading of geometry
+//   TString geo = gSystem->DynamicPathName("geometry.so");
+//   if ( !geo.Contains("Error" ) ) {
+//     std::cout << "Unloading geometry.so" << endl;
+//     gSystem->Unload( gSystem->DynamicPathName("geometry.so") );
+//   }
+
+
+  // Setup RNG seed and map all ROOT TRandom here
+  StarRandom::seed( rngSeed );
+  StarRandom::capture();
+  
+  //
+  // Create the primary event generator and insert it
+  // before the geant maker
+  //
+  //  StarPrimaryMaker *
+  _primary = new StarPrimaryMaker();
+  {
+    _primary -> SetFileName( "pythia8.starsim.root");
+    _primary -> SetVertex( 0.1, -0.1, 0.0 );
+    _primary -> SetSigma ( 0.1,  0.1, 30.0 );
+    chain -> AddBefore( "geant", _primary );
+  }
+
+  //
+  // Setup an event generator
+  //
+  double ckin3=3.0;
+  double ckin4=-1.0;
+  //Pythia8("pp:heavyflavor:D0jets", ckin3, ckin4 );
+  Pythia8("pp:minbias", ckin3, ckin4 );
+  command("call gstar_part"); 
+  
+  //Load geometry 
+  //-- don't seem to need to do this if same as passed to BFC above
+  //geometry("y2023");
+
+#if 1
+  //
+  // Setup the generator filter
+  //
+  //  filter = new StDijetFilter();
+
+  //filter = new FastJetFilter();
+ // _primary -> AddFilter( filter );
+
+  filter = new FcsJetFilter();
+  _primary -> AddFilter( filter );
+
+  // If set to 1, tracks will be saved in the tree on events which were
+  // rejected.  If the tree size is too big (because the filter is too
+  // powerful) you may want to set this equal to zero.  In which case
+  // only header information is saved for the event.
+  //_primary->SetAttr("FilterKeepAll",     int(1));
+
+  // By default, the primary maker enters an infinite loop and executes
+  // the event generator until it yields an event which passes the filter.
+  // The big full chain treats this as a single event.
+  //
+  // If you want the BFC to see an empty event, set the FilterSkipRejects
+  // attribute on the primary maker and give it the priveledge it needs
+  // to kill the event. 
+  //---  primary->SetAttr("FilterSkipRejects", int(1) ); // enables event skipping 
+  //---  chain->SetAttr(".Privilege",1,"StarPrimaryMaker::*" );
+#endif
+
+  //
+  // Setup cuts on which particles get passed to geant for
+  //   simulation.  (To run generator in standalone mode,
+  //   set ptmin=1.0E9.)
+  //                    ptmin  ptmax
+  _primary->SetPtRange  (0.0,  -1.0);         // GeV
+  //                    etamin etamax
+  _primary->SetEtaRange ( +1.0, +5.0 );
+  //                    phimin phimax
+  _primary->SetPhiRange ( 0., TMath::TwoPi() );
+  
+  
+  // 
+  // Setup a realistic z-vertex distribution:
+  //   x = 0 gauss width = 1mm
+  //   y = 0 gauss width = 1mm
+  //   z = 0 gauss width = 30cm
+  // 
+  _primary->SetVertex( 0., 0., 0. );
+  _primary->SetSigma( 0.1, 0.1, 30.0 );
+
+  
+  //
+  // Initialize primary event generator and all sub makers
+  //
+  _primary -> Init();
+
+  command("gkine -4 0");
+  command("gfile o pythia8.starsim.fzd");
+  
+
+  //
+  // Trigger on nevents
+  //
+  trig( nevents );
+
+  //
+  // Finish the chain
+  //
+  chain->Finish();
+
+  //
+  // EXIT starsim
+  //
+  command("call agexit");  // Make sure that STARSIM exits properly
+
+}
+// ----------------------------------------------------------------------------
+
